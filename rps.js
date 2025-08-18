b2df14c32f719a51b14c86108ac8ef1d5342c948
Swaraj Keshari <147552314+SwarajKeshari@users.noreply.github.com>
2025-08-18 23:56:37 +0530

    Add computer choice logic

---
+++ rps.js
@@ -0,0 +1,21 @@
+//
+
+function rps(){
+    console.log("Let the Game Begin");
+
+    const userChoicePrompt = prompt("Enter Rock/Paper/Scissors");
+    let userChoice = userChoicePrompt.toLocaleLowerCase();
+
+    let computerChoice;
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    if(randomNumber === 1){
+        computerChoice = "rock";
+    } else if(randomNumber === 2) {
+        computerChoice = "paper";
+    } else{
+        computerChoice = "scissors"
+    }
+
+    
+}
